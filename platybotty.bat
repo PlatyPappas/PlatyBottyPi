67a502a32e3f9648cea00c8d68de02e253d08e8f
ConnerPappasWebDev <63215665+PlatyPappas@users.noreply.github.com>
2021-08-01 00:00:39 -0700

    Update platybotty.bat

--- platybotty.bat
+++ platybotty.bat
@@ -2,5 +2,4 @@
 ECHO Starting up Twitch interface
 start node C:\Users\pappa\Documents\Projects\PlatyBottyPi\twitchInterface.js 
 ECHO Initiate python virtual environment
-C:\Users\pappa\Documents\Projects\PlatyBottyPi\.venv\Scripts\activate.bat && start python C:\Users\pappa\Documents\Projects\PlatyBottyPi\pluginLoader.py
-C:\Users\pappa\Documents\Projects\PlatyBottyPi\.venv\Scripts\deactivate.bat
+C:\Users\pappa\Documents\Projects\PlatyBottyPi\.venv\Scripts\activate.bat && start C:\Users\pappa\Documents\Projects\PlatyBottyPi\.venv\Scripts\python C:\Users\pappa\Documents\Projects\PlatyBottyPi\pluginLoader.py
